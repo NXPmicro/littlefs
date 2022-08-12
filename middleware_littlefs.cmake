eb2b2dfa451345960d1d97d82d7296585f1456c7
David Jurajda (nxa07326) <david.jurajda@nxp.com>
2022-08-12 12:32:16 +0200

    Apply MCUXpresso SDK 2.12.0 updates.

---
+++ middleware_littlefs.cmake
@@ -0,0 +1,14 @@
+#Description: LittleFS File System stack; user_visible: True
+include_guard(GLOBAL)
+message("middleware_littlefs component is included.")
+
+target_sources(${MCUX_SDK_PROJECT_NAME} PRIVATE
+    ${CMAKE_CURRENT_LIST_DIR}/lfs.c
+    ${CMAKE_CURRENT_LIST_DIR}/lfs_util.c
+)
+
+target_include_directories(${MCUX_SDK_PROJECT_NAME} PUBLIC
+    ${CMAKE_CURRENT_LIST_DIR}/.
+)
+
+
